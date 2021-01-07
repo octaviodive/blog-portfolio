95058583720b1e342a72de56ca587d14609c8266
Octavio Diaz <octaviodivework@gmail.com>
2021-01-07 11:02:04 -0400

    Added django-heroku

--- blog/settings.py
+++ blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,5 +132,5 @@ MEDIA_URL = '/images/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/projects/img')
 
-
+django_heroku.settings(locals())
 
