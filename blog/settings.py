af053beee344362ca96b3760a836132fb5990273
Octavio Diaz <octaviodivework@gmail.com>
2021-01-07 11:25:21 -0400

    Updated DEBUG

--- blog/settings.py
+++ blog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #SECRET_KEY = '!l&08k5(^=^mrk$fh5mr**00ru6(fblk8-3vr9ixxv953bo#7+'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('DEBUG_VALUE' == "True"))
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['octaviodiazwork.herokuapp.com']
 
