063572065ea9ab25097b5e842bcf262666080009
Octavio Diaz <octaviodivework@gmail.com>
2021-01-08 13:33:23 -0400

    Updated imagefield

--- projects/models.py
+++ projects/models.py
@@ -7,7 +7,7 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
-    image = models.FilePathField(path="/static/img/")
+    image = models.ImageField(upload_to='images/')
 
     def image_tag(self):
         if self.image:
