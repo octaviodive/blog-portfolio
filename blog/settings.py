a168652dd927a3365916c26d9bf3e619a9f9082b
Octavio Diaz <octaviodivework@gmail.com>
2021-01-08 18:29:14 -0400

    Updated statifiles for production3

--- blog/settings.py
+++ blog/settings.py
@@ -128,14 +128,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-MEDIA_URL = '/media/'
+MEDIA_URL = '/images/'
 
 
 
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
 
 
 django_heroku.settings(locals())
