b3ee1793f5e0fa927c988fac9276973e947bb6dd
Octavio Diaz <octaviodivework@gmail.com>
2021-01-11 10:31:35 -0400

    Change image field5

--- projects/models.py
+++ projects/models.py
@@ -7,15 +7,15 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
-    #image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='images/', black=True)
 
-    #def image_tag(self):
-        #if self.image:
-        #    return mark_safe('<img src="%s" style="width: 45px; height:45px;" />' % self.image)
-        #else:
-          #  return 'No Image Found'
+    def image_tag(self):
+        if self.image:
+            return mark_safe('<img src="%s" style="width: 45px; height:45px;" />' % self.image)
+        else:
+            return 'No Image Found'
 
-    #image_tag.short_description = 'Image'
+    image_tag.short_description = 'Image'
 
     def __str__(self):
         return self.title
