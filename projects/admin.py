7125466ced63d2592e4b5b5b1b87bd598952dc0c
Octavio Diaz <octaviodivework@gmail.com>
2021-01-11 11:17:00 -0400

    delete imagefield2

--- projects/admin.py
+++ projects/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Project
 # Register your models here.
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'image_tag')
+    list_display = ('title', 'description')
 
 admin.site.register(Project, ProjectAdmin)
