18453d3c7fdfb3de34a4fdbf98c3928e4abdd416
Octavio Diaz <octaviodivework@gmail.com>
2021-01-11 09:58:38 -0400

    Change image field2

--- projects/models.py
+++ projects/models.py
@@ -7,7 +7,8 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
-    image = models.ImageField(upload_to='images/')
+    image = models.FilePathField(path="/img")
+    #image = models.ImageField(upload_to='images/')
 
     def image_tag(self):
         if self.image:
