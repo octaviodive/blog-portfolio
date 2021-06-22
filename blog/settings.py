07da2ce0c032d727fe1d3f84d672f205b575eb95
octaviodive <76749707+octaviodive@users.noreply.github.com>
2021-06-22 12:37:30 -0400

    Update settings.py
    sk1

--- blog/settings.py
+++ blog/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY =
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+DEBUG = 
 
 ALLOWED_HOSTS = ['octaviodiazwork.herokuapp.com']
 
